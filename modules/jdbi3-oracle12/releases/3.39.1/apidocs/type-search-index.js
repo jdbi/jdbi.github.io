218db8e50f7fab5b5d211e5c293072f931a094d1
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2023-06-17 15:39:49 -0700

    Site checkin for project jdbi3 oracle12 (3.39.1)

---
+++ modules/jdbi3-oracle12/releases/3.39.1/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.jdbi.v3.oracle12","l":"OracleReturning"},{"p":"org.jdbi.v3.oracle12","l":"OracleReturning.ReturnParameters"}];updateSearchResults();
