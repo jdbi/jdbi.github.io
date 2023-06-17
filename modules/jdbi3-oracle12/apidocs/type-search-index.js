7a5beba0e03da4c63599e6f89e1e459dfa72da81
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2023-06-17 12:48:49 -0700

    remove manually deployed sites

--- modules/jdbi3-oracle12/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.jdbi.v3.oracle12","l":"OracleReturning"},{"p":"org.jdbi.v3.oracle12","l":"OracleReturning.ReturnParameters"}];updateSearchResults();
