5df7ff553090d2efd4ed7aed987d0497ddf710b6
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2023-06-24 21:26:40 -0700

    Site checkin for project jdbi3 - internal - documentation (3.40.0-SNAPSHOT)

--- modules/jdbi3-guava-cache/releases/3.39.1/testapidocs/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"org.jdbi.v3.cache.guava","c":"JdbiUsesGuavaCacheTest","l":"h2Extension"},{"p":"org.jdbi.v3.cache.guava","c":"JdbiUsesGuavaCacheTest","l":"JdbiUsesGuavaCacheTest()","u":"%3Cinit%3E()"},{"p":"org.jdbi.v3.cache.guava","c":"JdbiUsesGuavaCacheTest","l":"testUsingGuava()"}];updateSearchResults();
--- modules/jdbi3-guava-cache/releases/3.39.1/testapidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.jdbi.v3.cache.guava"}];updateSearchResults();
