f0a9ae16c303a66ec6c37cbc35def71b93925582
github-cd-action <github-cd-action@jdbi.org>
2024-01-03 06:48:09 +0000

    Site checkin for project jdbi3 oracle12 (3.43.0-SNAPSHOT)

--- modules/jdbi3-oracle12/apidocs/type-search-index.js
+++ modules/jdbi3-oracle12/apidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.jdbi.v3.oracle12","l":"OracleReturning"},{"p":"org.jdbi.v3.oracle12","l":"OracleReturning.ReturnParameters"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.jdbi.v3.oracle12","l":"OracleDatabaseRule"},{"p":"org.jdbi.v3.oracle12","l":"OracleReturning"},{"p":"org.jdbi.v3.oracle12","l":"OracleReturning.ReturnParameters"}];updateSearchResults();
